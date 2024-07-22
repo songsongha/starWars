55ed28652ddae41e13467ebc6d14231ff64ada8d
Song <songsongha@gmail.com>
2024-07-22 13:20:25 -0600

    Remove unused files, add query provider

--- src/views/UserList.tsx
+++ src/views/UserList.tsx
@@ -1,45 +1,37 @@
-import * as React from 'react';
-import { useQuery } from "react-query"
-import { getUserList } from "../utils/api"
-import Card from '@mui/material/Card';
+import * as React from 'react'
+import { useQuery } from 'react-query'
+import { getUserList } from '../utils/api'
+import Card from '@mui/material/Card'
 
 export function UserList() {
-    const {
-        data: users, 
-        error, 
-        isLoading 
-    } = useQuery('getUserList', getUserList )
+    const { data: users, error, isLoading } = useQuery('getUserList', getUserList)
 
-    console.log({users})
-    const cardData = React.useMemo(()=>{
+    console.log({ users })
+    const cardData = React.useMemo(() => {
         if (!users) return []
         const userArray = users.results
         // TODO: change any type
-        return userArray.map((result: any) =>{
-            const {name, birth_year, eye_color, gender, height} = result
+        return userArray.map((result: any) => {
+            const { name, birth_year, eye_color, gender, height } = result
             return {
                 name,
                 birth_year,
                 eye_color,
                 gender,
-                height: Number(result.height)/100
+                height: Number(result.height) / 100
             }
-    })
-},[])
-console.log({cardData})
+        })
+    }, [])
+    console.log({ cardData })
     if (isLoading) return <div> Fetching Users </div>
     if (error) {
-    console.log({error})
-    return <div> An error occured </div>
+        console.log({ error })
+        return <div> An error occured </div>
     }
     return (
-    <>
-        <h1>
-        Line-up User Profiles HELLO!
-        </h1>
-
-    </>
-
+        <>
+            <h1>Line-up User Profiles HELLO!</h1>
+        </>
     )
 }
 
